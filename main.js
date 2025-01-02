95af473ce32a1e92ebe656a692155634d828b498
sarav929 <ec2325508@edinburghcollege.ac.uk>
2025-01-02 11:56:04 +0000

    add logic to validate the input

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('all good');
+// return error if number is not within range or if it's not a number
+function isValidNumber(number) {
+  let errorMessage;
+  if (number < 1 || number > 10) {
+    return false;
+  } else if (isNaN(number)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// handle form submission and assign input value to number variable
+
+const form = document.getElementById('number-form');
+const input = document.getElementById('number-input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  guess = input.value;
+
+  // get result from validation
+
+  if (isValidNumber(guess)) {
+    console.log(`Your guess is ${guess}`);
+  } else {
+    console.log('Enter a number between 1 and 10');
+  }
+});
